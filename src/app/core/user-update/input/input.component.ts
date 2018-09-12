5804c3b2ef54fab82233956da8fafe6ed475d17e
fc <1344623045@qq.com>
2018-09-12 19:35:59 +0800

    回家啦

--- src/app/core/user-update/input/input.component.ts
+++ src/app/core/user-update/input/input.component.ts
@@ -12,6 +12,7 @@ export class InputComponent implements OnInit {
   ngOnInit() {
   }
 onkey(event: KeyboardEvent) {
+    console.log(event.key);
     this.values1 += (<HTMLInputElement>event.target).value + ' | ';
 }
 }
